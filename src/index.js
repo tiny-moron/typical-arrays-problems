462c40ee250cf6287f8480be5e2908530cbb0479
tiny-moron <68349383+tiny-moron@users.noreply.github.com>
2020-10-01 21:10:53 +0300

    complete tasks

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
-
 exports.min = function min (array) {
-  return 0;
+  if (isNaN(parseInt(array))) {
+    return 0;
+  }
+  let mini = array[0];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] < mini) {
+      mini = array[i];
+    }
+  }
+  return mini;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (isNaN(parseInt(array))) {
+    return 0;
+  }
+  let maxi = array[0];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] > maxi) {
+      maxi = array[i];
+    }
+  }
+  return maxi;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (isNaN(parseInt(array))) {
+    return 0;
+  }
+  let average = 0;
+  for (let i = 0; i < array.length; i++) {
+    average += array[i];
+  }
+  return average / array.length;
 }
